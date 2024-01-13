5d7c8cfc16a787462ac305f61a6d200bb4cf2866
Nikhil Raj <nikhilraj2061@gmail.com>
2024-01-14 00:39:30 +0530

    Update App.jsx
    Fixed the name.

--- src/App.jsx
+++ src/App.jsx
@@ -6,7 +6,7 @@ function App() {
   return (
     <>
       <div className='text-center text-5xl mt-[100px]'>
-      <h1 className=''>Welcome to EdLearnity...</h1>
+      <h1 className=''>Welcome to EdLernity...</h1>
       <h3>Coming soon...</h3>
       </div>
       <div className="ring " >Loading...
