59703268993baebdb3d6e05af8f0533fb7453b80
rishuk524 <rishuk524@gmail.com>
2024-01-31 23:06:02 +0530

    added two component for error handling

--- src/Components/Auth/UpdatePassword/UpdatePassword.jsx
+++ src/Components/Auth/UpdatePassword/UpdatePassword.jsx
@@ -1,29 +1,13 @@
 import React from "react";
-import {
-  CardBody,
-  CardFooter,
-  Button,
-} from "@material-tailwind/react";
-import InputButton from "../../Input/InputButton";
+import PasswordComponent from "./PasswordComponent/PasswordComponent";
+import ErrorComponent from "./ErrorComponent/ErrorComponent";
 
 function UpdatePassword() {
-  
-  const buttonColor = {
-    background: "#1539cf",
-    color: "white",
-    borderRadius: '15px'
-  };
-
+    let isSucess=false
   return (
-    <div className="flex justify-center items-center xl:w-2/5 md:w-2/4">
+    <div className="flex justify-center items-center xl:w-2/4 md:w-2/4 pb-24">
         <div className="p-6 w-full flex justify-center items-center flex-col">
-          <CardBody className="flex flex-col gap-4 w-full">
-            <InputButton fullWidth label="New Password" type="password"/>
-            <InputButton fullWidth label="Confirm Password" type="password"/>
-          </CardBody>
-          <CardFooter className="pt-0 flex flex-col w-[80%]">
-            <Button style={buttonColor}>Submit</Button>
-          </CardFooter>
+          {isSucess ? <PasswordComponent/> : <ErrorComponent/>}
         </div>
     </div>
   );
