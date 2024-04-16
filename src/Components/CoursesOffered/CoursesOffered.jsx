53fe612b30526fac7e28e3930444ca5718963eb4
Ishu Kumar <ishu.kumar0007@gmail.com>
2024-04-16 10:17:39 +0530

    "Updated course starting price from ₹399 to ₹499 in CoursesOffered component"

--- src/Components/CoursesOffered/CoursesOffered.jsx
+++ src/Components/CoursesOffered/CoursesOffered.jsx
@@ -109,7 +109,7 @@ function CoursesOffered() {
             <p class="text-base font-semibold text-center text-gray-600">Explore Our Courses</p>
             <p class="mt-6 flex items-baseline justify-center gap-x-2">
             Strating from 
-              <span class="text-5xl font-bold tracking-tight text-gray-900">₹399</span>
+              <span class="text-5xl font-bold tracking-tight text-gray-900">₹499</span>
               <span class="text-sm font-semibold leading-6 tracking-wide text-gray-600">INR</span>
             </p>
             <span  class="mt-10 block w-full rounded-md bg-gray-900 px-3 py-2 text-center text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600">Explore</span>
